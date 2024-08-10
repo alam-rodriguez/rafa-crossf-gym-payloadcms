af17adc6324bee28eebeb753ed4f8b6e7b92ec7b
alamRodriguez <alamrd2016@gmail.com>
2024-08-09 20:30:37 -0400

    ecmascript module added

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express'
-import payload from 'payload'
+import express from "express";
+import payload from "payload";
 
-require('dotenv').config()
-const app = express()
+import "dotenv/config";
+const app = express();
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
+app.get("/", (_, res) => {
+  res.redirect("/admin");
+});
 
-import cors  from "cors";
+import cors from "cors";
 app.use(cors());
 
 const start = async () => {
@@ -18,13 +18,13 @@ const start = async () => {
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
   // Add your own express routes here
 
-  app.listen(3000)
-}
+  app.listen(3000);
+};
 
-start()
+start();
